7d32a1d753d8c2e5c6da5d188d258ce920010d49
Ivanias <56035889+Ivanias@users.noreply.github.com>
2021-05-25 10:23:34 +0300

    Add files via upload

--- Сoordinates on the plan/imagePreparation.py
+++ Сoordinates on the plan/imagePreparation.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import sys
+import configparser
 
 def truckDetection (image,fon,x,y,w,h,kheight,kwidth):
     # Нахождение объекта на исходном изображении
@@ -24,7 +25,7 @@ def truckDetection (image,fon,x,y,w,h,kheight,kwidth):
             scores = detection[5:]
             class_id = np.argmax(scores)
             confidence = scores[class_id]
-            if confidence > 0.5:
+            if confidence > 0.3:
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 # Координаты прямоугольника
@@ -37,7 +38,7 @@ def truckDetection (image,fon,x,y,w,h,kheight,kwidth):
                 confidences.append(float(confidence))
                 class_ids.append(class_id)
 
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.4, 0.3)
     font = cv2.FONT_HERSHEY_PLAIN
     # Отображение прямоугольников
     for i in range(len(boxes)):
@@ -47,16 +48,22 @@ def truckDetection (image,fon,x,y,w,h,kheight,kwidth):
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
             cv2.circle(image, (int((x + x + w) / 2), int((y + y + h) / 2)), 3, (0, 255, 0), -1)
     #cv2.imshow("image", image)
-    cv2.rectangle(fon, (int(x/kwidth), int(y/kheight)), (int((x + w)/kwidth), int((y + h)/kheight)), (0, 255, 0), 2) # Рисование на белом листе прямоугольника, ограничивающий объект
+    cv2.rectangle(fon, (int(x/kwidth), int((y+h)/kheight)), (int((x + w)/kwidth), int((y + h)/kheight)), (0, 255, 0), 2) # Рисование на белом листе прямоугольника, ограничивающий объект
     #cv2.imshow("fon", fon)
     return image,fon,x,y,w,h
 
 def imagePreparation (img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
-    imgThreshold = cv2.Canny(imgBlur, 200, 200)
-    kernel = np.ones((5, 5))
-    imgDial = cv2.dilate(imgThreshold, kernel, iterations=2)
+    imgThreshold = cv2.Canny(imgBlur, 50, 400)
+    return imgThreshold
+
+def imagePreparation1 (img):
+    #cv2.imshow("test1", img)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)
+    imgThreshold = cv2.Canny(imgBlur, 50, 200)
+    #cv2.imshow("test2", imgThreshold)
     return imgThreshold
 
 def domatrix(fon,matrix,widthImg,heightImg):
@@ -64,23 +71,24 @@ def domatrix(fon,matrix,widthImg,heightImg):
     #cv2.imshow("Warp white list", newfon)
     return newfon
 
-def coordinatesOnThePlan(biggest1,newfon,plan):
-    x11 = int(str(biggest1[0])[2:6])
-    x12 = int(str(biggest1[1])[2:6])
-    x21 = int(str(biggest1[2])[2:6])
-    x22 = int(str(biggest1[3])[2:6])
-    y11 = int(str(biggest1[0])[6:-2])
-    y12 = int(str(biggest1[1])[6:-2])
-    y21 = int(str(biggest1[2])[6:-2])
-    y22 = int(str(biggest1[3])[6:-2])
+def coordinatesOnThePlan(biggest1,newfon,finalPlan):
+    x11 = int(str(biggest1[0])[2:5])
+    x12 = int(str(biggest1[1])[2:5])
+    x21 = int(str(biggest1[2])[2:5])
+    x22 = int(str(biggest1[3])[2:5])
+    y11 = int(str(biggest1[0])[5:-2])
+    y12 = int(str(biggest1[1])[5:-2])
+    y21 = int(str(biggest1[2])[5:-2])
+    y22 = int(str(biggest1[3])[5:-2])
+    klength=780/640
+    kwidth=250/480
     cv2.circle(newfon, (int(((x11 + x12) / 2 + (x21 + x22) / 2) / 2), int(((y11 + y21) / 2 + (y12 + y22) / 2) / 2)), 3,
                (0, 0, 0), -1)
-    cv2.circle(plan, (int(((x11 + x12) / 2 + (x21 + x22) / 2) / 2), int(((y11 + y21) / 2 + (y12 + y22) / 2) / 2)), 3,
-               (0, 0, 0), -1)
-    print('Координаты центра объекта на повёрнутом изображении', (((x11 + x12) / 2 + (x21 + x22) / 2) / 2),
-          (((y11 + y21) / 2 + (y12 + y22) / 2) / 2))
-    cv2.imshow("Plan", plan)
-    return plan
+    cv2.circle(finalPlan, (int((((x11 + x12) / 2 + (x21 + x22) / 2) / 2)*klength+304), int(((((y11 + y21) / 2 + (y12 + y22) / 2) / 2))*kwidth+67)), 3,
+               (0, 255, 0), -1)
+    print('Координаты центра объекта на складе:', int((((x11 + x12) / 2 + (x21 + x22) / 2) / 2)*klength+304), int(((((y11 + y21) / 2 + (y12 + y22) / 2) / 2))*kwidth+67))
+    cv2.imshow("finalPlan", finalPlan)
+    return finalPlan
 
 
 def reorder(myPoints):
@@ -93,7 +101,6 @@ def reorder(myPoints):
     diff = np.diff(myPoints, axis=1)
     myPointsNew[1] = myPoints[np.argmin(diff)]
     myPointsNew[2] = myPoints[np.argmax(diff)]
-
     return myPointsNew
 
 def biggestContour(contours):
@@ -115,5 +122,42 @@ def drawRectangle(img, biggest, thickness):
     cv2.line(img, (biggest[0][0][0], biggest[0][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 255), thickness)
     cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[2][0][0], biggest[2][0][1]), (0, 255, 255), thickness)
     cv2.line(img, (biggest[3][0][0], biggest[3][0][1]), (biggest[1][0][0], biggest[1][0][1]), (0, 255, 255), thickness)
-
     return img
+
+def getCoordinates (countX,countY1,countY2,contours1):
+    for i in range(len(contours1[0])):
+        countX = countX + contours1[0][i][0][0]
+        if i==0:
+            countY1=contours1[0][i][0][1]
+            continue
+        if contours1[0][i][0][1]/contours1[0][0][0][1]<=1.15:
+            if contours1[0][i][0][1]/contours1[0][0][0][1]>= 0.85:
+                countY1 = countY1 + contours1[0][i][0][1]
+            else:
+                countY2=countY2+contours1[0][i][0][1]
+                if i != len(contours1[0]) - 1:
+                    continue
+        else:
+            countY2 = countY2 + contours1[0][i][0][1]
+        if i == len(contours1[0])-1:
+            countX=countX/len(contours1[0])
+            if countY1>countY2:
+                countYT=countY1
+                countY1=countY2
+                countY2=countYT
+            countY1=countY1/(len(contours1[0])/2)
+            countY2=countY2/(len(contours1[0])/2)
+
+    coord1=[countX,countY1]
+    coord2=[countX,countY2]
+    return coord1,coord2
+
+def getRectangle (coord1,coord2,newfon):
+    config = configparser.ConfigParser()  # создаём объекта парсера
+    config.read("settings.ini")  # читаем конфиг
+    sizeXm=float(config["forklift positioning"]["lengthForklift"])
+    sizeYm=float(config["forklift positioning"]["widthForklift"])
+    sizePX=((coord2[1]-coord1[1])/sizeXm)*sizeYm
+    cv2.rectangle(newfon, (int(coord1[0]), int(coord1[1])), (int(coord2[0]+sizePX), int(coord2[1])), (0, 255, 0), 2) # Рисование на белом листе прямоугольника, ограничивающий объект
+    #cv2.imshow("inew", newfon)
+    return newfon
